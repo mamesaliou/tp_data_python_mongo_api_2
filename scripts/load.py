495323f74ac71c7fd0f18b14187d1968d7e16e35
M.S.C. <77129311+mamesaliou@users.noreply.github.com>
2025-03-08 04:56:39 +0000

    livraison : chargemeent des données dans MongoDB

--- scripts/load.py
+++ scripts/load.py
@@ -1,13 +1,3 @@
-# from pymongo import MongoClient
-
-# def load_data(df, db_name, collection_name):
-#     client = MongoClient("mongodb://localhost:27017/")
-#     db = client[db_name]
-#     collection = db[collection_name]
-    
-#     data_dict = df.to_dict("records")
-#     collection.insert_many(data_dict)
-
 from pymongo import MongoClient, errors
 
 def load_data(df, db_name, collection_name):
@@ -19,19 +9,11 @@ def load_data(df, db_name, collection_name):
         data_dict = df.to_dict("records")
         collection.insert_many(data_dict)
         
-        print("Data inserted successfully")
-    except errors.ServerSelectionTimeoutError as sste:
-        print("Server selection error: Unable to connect to MongoDB server.")
-        print(sste)
-    except errors.ConnectionFailure as cf:
-        print("Connection failure: Failed to connect to MongoDB server.")
-        print(cf)
-    except errors.PyMongoError as pme:
-        print("PyMongo error occurred.")
-        print(pme)
+    print("Données chargées avec succès dans MongoDB.")
     except Exception as e:
-        print("An unexpected error occurred.")
-        print(e)
+        print(f"Erreur lors du chargement des données dans MongoDB : {e}")
 
 # Example usage
 # load_data(your_dataframe, 'your_db_name', 'your_collection_name')
+# Utiliser mongosh pour vérifier l'ingestion des données dans MongoDB: 
+# docker exec -it mongodb mongosh
